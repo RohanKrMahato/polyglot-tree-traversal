933e59becf7f108b35efbc48dac823e79276549d
Rohan Kumar Mahato <rohankumarmahato345@gmail.com>
2024-12-21 17:41:17 +0530

    minor fix

--- rootNode.py
+++ rootNode.py
@@ -5,5 +5,5 @@ metacall_load_from_file('node', ['middleNode.js']);
 currentNode=1;
 
 metacall('traverse_middleNode',2*currentNode);
-print(currentNode," ",end="",flush=True)
+print(currentNode,end=" ",flush=True)
 metacall('traverse_middleNode',2*currentNode+1);
